837980bb4b9331162e27b49c08ee1fb913b88c95
naruto <114555545+vijay143v@users.noreply.github.com>
2022-09-28 11:04:19 +0530

    Add files via upload

---
+++ ex4.sh
@@ -0,0 +1,14 @@
+echo "enter three numbers:"
+read a b c
+
+if [ $a -lt $b && $a -lt $c ]
+then
+echo "$a is smaller then $b , $c"
+fi
+elif [ $b -lt $a && $b -lt $c ]
+then
+echo "$b is smaller than $a , $c"
+file
+else
+echo "$c is smaller than $a , $b"  
+
